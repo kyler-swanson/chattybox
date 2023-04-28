9ca59a1da59dc7f112cc46abb23842c39f054556
kyler-swanson <kylerswanson@gmail.com>
2023-04-28 18:38:45 -0400

    Change icon and add favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
-import { BsChatSquareDots } from 'react-icons/bs';
+import { HiChatAlt2 } from 'react-icons/hi';
 import Login from './Login';
 
 const Logo = () => (
   <div className='flex items-center'>
-    <BsChatSquareDots className='text-4xl text-violet-950' />
-    <h1 className='text-2xl font-bold ml-4'>ChattyBox</h1>
+    <HiChatAlt2 className='text-5xl text-violet-950' />
+    <h1 className='text-2xl font-bold ml-2'>ChattyBox</h1>
     <small className='ml-4 text-sm text-gray-500 hidden sm:block'>Join the conversation!</small>
   </div>
 );
