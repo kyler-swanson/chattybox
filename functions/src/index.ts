adb71ee2f19ea1b98df07b6be4fde2ce5d8ccae5
kyler-swanson <kylerswanson@gmail.com>
2023-04-30 13:12:33 -0400

    Add function to update replies on mod

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
 import BadWordsFilter = require('bad-words');
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 const filter = new BadWordsFilter();
 
+// cloud function to filter profanity
 exports.moderator = functions.firestore.document('/messages/{messageId}').onWrite((change) => {
   const snap = change.after;
 
@@ -28,6 +30,25 @@ const containsProfanity = (text: string) => {
 const filterProfanity = (text: string) => {
   return filter.clean(text);
 };
+
+exports.updateModifiedReplies = functions.firestore.document('/messages/{messageId}').onUpdate((change) => {
+  const before = change.before.data();
+  const after = change.after.data();
+
+  if (before?.message !== after?.message) {
+    const messagesRef = admin.firestore().collection('messages');
+    const query = messagesRef.where('repliesTo.id', '==', before?.id);
+    return query.get().then((messages) => {
+      messages.forEach((snap) => {
+        snap.ref.update({
+          'repliesTo.message': '(deleted)'
+        });
+      });
+    });
+  }
+
+  return null;
+});
 
 // remove messages older than 1 hour
 exports.cleaner = functions.pubsub.schedule('every 1 hours').onRun(async () => {
